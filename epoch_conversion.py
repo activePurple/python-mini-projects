23074cfcd66577c7c1f8bc78a809ac7c8adc8d61
Tyler O'Neil <tyleroneil523@gmail.com>
2023-12-15 22:54:19 -0900

    Update epoch_conversion.py
    Created userInput() function to serve for gathering information and some calculations.

--- epoch_conversion.py
+++ epoch_conversion.py
@@ -8,14 +8,13 @@ import math
 # Functions for getting currents days epoch Beginning of day
 def unixDay(time = time.time()):
     start_of_day = math.floor(time / 86400)*86400
+    
+    current_epoch = time
+    
     end_of_day = start_of_day + 86399
-    output = f'\nStart of Day: {start_of_day}\nEnd of Day: {end_of_day}\n'
+    
+    output = f'\nStart of Day: {start_of_day}\nEnd of Day: {end_of_day}\nCurrent Epoch: {current_epoch}'
     return output
-  
-# User input get Epoch from queried date and time
-def getUnixTime(queryTime):
-    for i in range(len(queryTime)):
-        return time.mktime(time.strptime(queryTime[i], "%m %d %Y %H:%M:%S"))
     
     
 # Daylight saving time always starts on the second Sunday in March 
@@ -23,16 +22,14 @@ def getUnixTime(queryTime):
 def daylightSavingsUs():
     pass
 
-# User input and delimiter
-queryTime = input('\nEnter time as DD MM YYYY HH:MM:SS: ')
-userInput = queryTime.split(': ')      
-      
-
-print(f'\nEpoch in Local Time: {getUnixTime(userInput)}\n')
-
-print(unixDay(getUnixTime(userInput)))
-
-
+# User input as function
+def userInput():
+  queryTime = input('\nEnter time as DD MM YYYY HH:MM:SS: ')
+  queryDate = queryTime.split(': ')
+  def getUnixTime(queryDate):
+    for i in range(len(queryDate)):
+        return time.mktime(time.strptime(queryDate[i], "%m %d %Y %H:%M:%S"))
+  return getUnixTime(queryDate)   
 
 
-
+print(unixDay(userInput()))
